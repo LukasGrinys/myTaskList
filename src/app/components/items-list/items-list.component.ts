fd8782c3ca9e4a4543c4fb06819126049a40df84
LukasGrinys <lukas.grinys@gmail.com>
2020-06-28 17:47:56 +0300

    Empty task bug fixed

--- src/app/components/items-list/items-list.component.ts
+++ src/app/components/items-list/items-list.component.ts
@@ -15,37 +15,68 @@ export class ItemsListComponent implements OnInit {
   constructor() {
     this.tasks = []
   }
+
   ngOnInit(): void {
+    this.loadStoredData();
+  }
+
+  loadStoredData():void {
     const storedList = localStorage.getItem('toDoList');
     if (storedList) {
-      this.tasks = JSON.parse(storedList).tasks;
+      const cachedData = JSON.parse(storedList).tasks;
+      let tasks = [];
+      cachedData.forEach( (todo) => {
+        let newTask = {
+          date : todo['date'],
+          task : todo['task'],
+          completed : todo['completed'],
+          isBeingDeleted : false
+        }
+        tasks.push(newTask);
+      }); 
+      this.tasks = tasks;
     }
   }
-  checkItem(index) {
+
+  checkItem(index):void {
     let currentState = this.tasks[index].completed;
     this.tasks[index].completed = !currentState;
     this.saveList();
   }
+
   returnRelativeTime(index) {
     let date = this.tasks[index].date;
     return dayjs(date).fromNow();
   }
-  deleteItem(index) {
+
+  deleteItem(index):void {
     this.tasks[index].isBeingDeleted = true;
     setTimeout( () => {
       this.tasks.splice(index,1);
       this.saveList();
     }, 1000)
   }
-  addTask = (task) => {
+
+  addTask(task):void {
     this.tasks.push(task);
     this.saveList();
   }
-  saveList = () => {
-    let cache = JSON.stringify({ tasks : this.tasks});
+
+  saveList():void {
+    let tasks = [];
+    this.tasks.forEach( (todo) => {
+      let taskData = {
+        date : todo.date,
+        task : todo.task,
+        completed : todo.completed
+      };
+      tasks.push(taskData);
+    });
+    let cache = JSON.stringify({ tasks });
     localStorage.setItem('toDoList', cache);
   }
-  renderTimeIcon = (index) => {
+
+  renderTimeIcon(index) {
     let date = this.tasks[index].date;
     let now = dayjs(dayjs().format());
     let difference = now.diff(date, 'day');
